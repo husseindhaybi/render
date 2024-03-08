0c82494ffd5d53bc8b055583e8431f9f79976c61
hussein dhaybi <you@example.com>
2024-03-08 15:14:17 +0200

    second commit

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
 // your code
+app.get('/hussein', function (req, res) {
+    let data  =[{name:'hussein',id:12},{name:'mazn',id:11},{name:'moataz',id:15}]
+    res.send(data)
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
